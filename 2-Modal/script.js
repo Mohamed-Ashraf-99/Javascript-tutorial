54f80cd915050f18e8fb962856f63f1a34b4a67d
Mohamed-Ashraf-99 <mohamedashrafita@gmail.com>
2023-06-10 23:32:02 +0200

    Upload

---
+++ 2-Modal/script.js
@@ -0,0 +1,29 @@
+'use strict';
+const showModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', openModal);
+};
+
+close.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.code === 'Escape') {
+    closeModal();
+  }
+})
